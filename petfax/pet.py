d56105ab0cdb053b5804197041b930768d5949aa
Daniel Flores <DannniF>
2024-04-04 17:05:04 -0700

    completed in class

--- petfax/pet.py
+++ petfax/pet.py
@@ -1,24 +1,46 @@
-from flask import (Blueprint, render_template)
-import json
+from flask import ( Blueprint, render_template ) 
+import json 
 
 pets = json.load(open('pets.json'))
-print(pets)
 
-bp = Blueprint('pet',__name__,url_prefix="/pets")
+bp = Blueprint('pet', __name__, url_prefix="/pets")
 
 @bp.route('/')
-def index ():
-    return render_template('index.html', pets=pets)
-
-# @bp.route('/<int:id>')
-# def show_pet(id):
-# 	return render_template('show_pet.html', pet=pets[id])
-
-# @bp.route('/<int:id>')
-# def show_pet(id):
-# 	return render_template('show_pet.html', pet=pets[id])
-
+def index(): 
+    return render_template('pets/index.html', pets=pets)
 
 @bp.route('/<int:id>')
-def show_pet(id):
-	return render_template('show_pet.html', pet=next(filter(lambda p: p['pet_id'] == id, pets)))
+def show(id): 
+    pet = pets[id - 1]
+    return render_template('pets/show.html', pet=pet)
+
+
+
+# from flask import (Blueprint, render_template)
+# import json
+
+# pets = json.load(open('pets.json'))
+# print(pets)
+
+# bp = Blueprint('pet',__name__,url_prefix="/pets")
+
+# @bp.route('/')
+# def index ():
+#     return render_template('index.html', pets=pets)
+
+# # @bp.route('/<int:id>')
+# # def show_pet(id):
+# # 	return render_template('show_pet.html', pet=pets[id])
+
+# # @bp.route('/<int:id>')
+# # def show_pet(id):
+# # 	return render_template('show_pet.html', pet=pets[id])
+
+
+# @bp.route('/<int:id>')
+# def show_pet(id):
+# 	return render_template('show_pet.html', pet=next(filter(lambda p: p['pet_id'] == id, pets)))
+
+# @bp.route('/facts')
+# def facts():
+#       return render_template('facts.html')
